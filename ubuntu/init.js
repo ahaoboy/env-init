12b7f0faed4098b2ebe89726f5c2d6bb7881b2d7
阿豪boy <504595380@qq.com>
2021-12-18 23:38:49 +0800

    fix: add 2110

--- ubuntu/init.js
+++ ubuntu/init.js
@@ -20,43 +20,6 @@ if (!fs.existsSync(tool_dir)) {
   await $`mkdir tool`;
 }
 cd(tool_dir);
-await $`sudo apt update -y`;
-await $`sudo apt upgrade -y`;
-
-const common_str = [
-  "net-tools",
-  "openssh-server",
-  "clang",
-  "lld",
-  "gdb",
-  "default-jre",
-  "net-tools",
-  "gcc",
-  "g++",
-  "cmake",
-  "openssl",
-  "libssl-dev",
-  "cmake",
-  "git",
-  "libgtk2.0-dev",
-  "pkg-config",
-  "build-essential",
-  "nasm",
-  "wget",
-  "curl",
-  "xsel",
-  "zip",
-  "unzip",
-  "tar",
-  "software-properties-common",
-  "apt-transport-https",
-  "python3",
-  "python3-pip",
-  "fonts-firacode",
-  "clang-format",
-  "zsh",
-];
-await $`sudo apt install ${common_str} -y`;
 
 try {
   await $`google-chrome --version`;
