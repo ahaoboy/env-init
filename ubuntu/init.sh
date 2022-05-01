dad37cac766d3f92f0c796c369590a62dd0678d7
阿豪boy <504595380@qq.com>
2022-05-01 18:51:10 +0800

    fnm

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,13 +1,22 @@
 sudo apt install curl -y
 
 echo "=========install nodejs========="
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-source ~/.bashrc
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
-nvm list 
-nvm install 16   # or 10.10.0, 8.9.1, etc 
-nvm use 16
-npm i pnpm yarn -g
+# source ~/.bashrc
+
+# nvm list 
+# nvm install 16   # or 10.10.0, 8.9.1, etc 
+# nvm use 16
+
+curl -fsSL https://fnm.vercel.app/install | bash
+source ~/.bashrc
+fnm install 16
+fnm use 16 
+fnm default 16
+
+
+npm i pnpm yarn trash-cli -g
 pnpm i serve -g
 pnpm i zx -g
 echo "=========finish nodejs========="
