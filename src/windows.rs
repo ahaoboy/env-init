02b3f9cb22b6d7949567d2db8d50ff66124030e1
ahaoboy <504595380@qq.com>
2026-06-22 11:13:19 +0800

    windows

---
+++ src/windows.rs
@@ -0,0 +1,34 @@
+use anyhow::Result;
+use log::info;
+
+use crate::{
+    base::{append_file, replace_file},
+    common::{get_msys64_root, log_operation},
+};
+
+/// Configure Windows/MSYS2 shell and path settings.
+#[cfg(windows)]
+pub fn windows() -> Result<()> {
+    log_operation("windows: configuring MSYS2 settings");
+    info!("Configuring Windows/MSYS2 settings...");
+
+    let shell_path = get_msys64_root()? + "/etc/shells";
+    if !std::fs::read_to_string(&shell_path)
+        .unwrap_or_default()
+        .contains("/usr/bin/fish")
+    {
+        append_file(&shell_path, "/usr/bin/fish")?;
+    }
+
+    replace_file(
+        "C:/msys64/msys2_shell.cmd",
+        "rem set MSYS2_PATH_TYPE=inherit",
+        "set MSYS2_PATH_TYPE=inherit",
+    )?;
+    replace_file(
+        "C:/msys64/etc/nsswitch.conf",
+        "db_home: cygwin desc",
+        "db_home: windows",
+    )?;
+    Ok(())
+}
